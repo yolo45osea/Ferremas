013f38aacfd09fe6d0b009bc5d6d7876e92ef691
benjamin romero <133557177+yolo45osea@users.noreply.github.com>
2025-05-07 22:32:29 -0400

    Update settings.py

--- Ferremas/settings.py
+++ Ferremas/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'Ferremas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ferremas_78nc',
+        'USER': 'ferremas_78nc_user',
+        'PASSWORD': 'UEIDWpAjjFMdO1zuYZgqRcphHyRlKwSX',
+        'HOST': 'dpg-d0e1ds15pdvs73alb3b0-a',
+        'PORT': '5432',
     }
 }
 
