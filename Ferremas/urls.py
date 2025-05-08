95aa746dbf00edaa08feacc0ce9f7655625aed45
benjamin romero <133557177+yolo45osea@users.noreply.github.com>
2025-05-07 22:57:56 -0400

    Update urls.py

--- Ferremas/urls.py
+++ Ferremas/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('principal.urls')),
-    path('pagos', include('pagos.urls'))
+    path('pagos/', include('pagos.urls'))
 ]
